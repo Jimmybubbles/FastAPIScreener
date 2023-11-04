01c4324b95d0773ff0bf842042b2c29069ea4243
James <james.anthony.russell36@gmail.com>
2023-11-04 12:38:41 +1100

    api endpoint at yahoo is offline.

--- main.py
+++ main.py
@@ -1,31 +1,65 @@
-from typing import Union
+import models
+import yfinance
 from fastapi.templating import Jinja2Templates
-
-from fastapi import FastAPI, Request
+from models import Stock
+from fastapi import FastAPI, Request, Depends, BackgroundTasks
+from database import SessionLocal, engine
+from pydantic import BaseModel
+from sqlalchemy.orm import Session
 
 app = FastAPI()
 
+models.Base.metadata.create_all(bind=engine)
+
 templates = Jinja2Templates(directory="templates")
 
-"""
-Displays the dashboard for the homepage
-"""
+
+class StockRequest(BaseModel):
+    symbol: str
+    
+def get_db():
+    try:
+        # create new db session
+        db = SessionLocal()
+        yield db
+    finally:
+        # close db session
+        db.close()
 
 @app.get("/")
 def dashboard(request: Request):
     return templates.TemplateResponse("dashboard.html", {
-        "request": request,
-        "somevar": 3
+        "request": request
     } )
 
-    """creates a stock and stores it in the database
+def fetch_stock_data(id: int):
+    db = SessionLocal()
+    stock = db.query(Stock).filter(Stock.id == id).first()
+    
+    yahoo_data = yfinance.Ticker(stock.symbol)
+    
+    stock.ma200 = yahoo_data.info['twoHundredDayAverage']
+    stock.ma50 = yahoo_data.info['fiftyDayAverage']
+    stock.price = yahoo_data.info['previousClose']
+    stock.forward_pe = yahoo_data.info['forwardPE']
+    stock.forward_eps = yahoo_data.info['forwardEps']
+    stock.dividend_yield = yahoo_data.info['dividendYield'] * 100
+    
 
-    Returns:
-        string: stock
-    """
+    db.add(stock)
+    db.commit()    
 
 @app.post("/stock")
-def create_stock():
+def create_stock(stock_request: StockRequest, background_tasks: BackgroundTasks, db: Session = Depends(get_db)):
+    
+    stock = Stock()
+    stock.symbol = stock_request.symbol
+    
+    db.add(stock)
+    db.commit()
+    
+    background_tasks.add_task(fetch_stock_data, stock.id)
+    
     return {
         "code":"success",
         "message":"Stock Created"
---
+++ news_webscrape_country_products.py
@@ -0,0 +1,20 @@
+"""
+This is going to find out the commodities that are produced in a country
+
+find and api for this 
+
+this is the use case
+
+news from a country is promoted on twitter etc pull the reference from the other media outlets
+
+you could like have a scanner that is continually scanning the news sites for keywords - country and then /political/econimic news 
+
+the look for positive/negative economy impact and then algo trades on positive/negative news for futures points against chart indicators.
+indicators are done so build scanner for news first.
+
+then list all the commodities sold and bought. 
+
+list them in a table with the current futures price on the commodity.
+
+
+"""
